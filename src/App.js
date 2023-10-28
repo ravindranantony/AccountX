3f1d0d20e6e44a39fb81f3548e64f890eb6089c3
saravanan selvam <saravanans541@gmail.com>
2023-10-28 13:10:03 +0530

    PPP009/personal details added

--- src/App.js
+++ src/App.js
@@ -81,7 +81,6 @@ function CompanyProfile() {
       logo: null
   });
 
-}
   const handleInputChange = (event) => {
       const { name, value } = event.target;
       setCompanyProfileData(prevData => ({
@@ -148,7 +147,7 @@ function CompanyProfile() {
           </div>
       </div>
   );
-
+}
 
 
 function Addresses() {
@@ -156,59 +155,54 @@ function Addresses() {
 }
 
 function PersonalDetails() {
-  
-  ```jsx
-  <div className="company-profile">
-    <h2>Company Profile</h2>
-    <div className="grid-row">
-      <div className="input-group">
-        <input type="text" name="companyType" placeholder="Company Type" value={companyProfileData.companyType} onChange={handleInputChange} />
+  const [personalData, setPersonalData] = useState({
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    fatherName: '',
+    motherName: '',
+    gender: '',
+    dateOfBirth: '',
+    PAN: '',
+    AADHAR: '',
+    DIN: '',
+    state: '',
+    nationality: '',
+    designation: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPersonalData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  return (
+    <div className="personal-details">
+      <h2>Personal Details</h2>
+      <div className="input-group" style={{ display: 'flex', flexDirection: 'row' }}>
+        <input type="text" name="firstName" placeholder="First Name" onChange={handleInputChange} />
+        <input type="text" name="middleName" placeholder="Middle Name" onChange={handleInputChange} />
+        <input type="text" name="lastName" placeholder="Last Name" onChange={handleInputChange} />
+        <input type="text" name="fatherName" placeholder="Father Name" onChange={handleInputChange} />
       </div>
-      <div className="input-group">
-        <input type="radio" name="registrationStatus" value="Registered" checked={companyProfileData.registrationStatus === 'Registered'} onChange={handleInputChange} /> Registered
-        <input type="radio" name="registrationStatus" value="Unregistered" checked={companyProfileData.registrationStatus === 'Unregistered'} onChange={handleInputChange} /> Unregistered
+      <div className="input-group" style={{ display: 'flex', flexDirection: 'row' }}>
+        <input type="text" name="motherName" placeholder="Mother Name" onChange={handleInputChange} />
+        <input type="text" name="gender" placeholder="Gender" onChange={handleInputChange} />
+        <input type="date" name="dateOfBirth" placeholder="Date of Birth" onChange={handleInputChange} />
+        <input type="text" name="PAN" placeholder="PAN" onChange={handleInputChange} />
       </div>
-      <div className="input-group">
-        <input type="text" name="companyID" placeholder="Company ID" onChange={handleInputChange} />
-      </div>
-    </div>
-    <div className="grid-row">
-      <div className="input-group">
-        <input type="text" name="GSTNumber" placeholder="GST Number" onChange={handleInputChange} />
-      </div>
-      <div className="input-group">
-        <input type="text" name="businessTradeName" placeholder="Business Trade Name" onChange={handleInputChange} />
-      </div>
-      <div className="input-group">
-        <input type="text" name="businessLegalName" placeholder="Business Legal Name" onChange={handleInputChange} />
-      </div>
-    </div>
-    <div className="grid-row">
-      <div className="input-group">
-        <input type="text" name="mobileNumber" placeholder="Mobile Number" onChange={handleInputChange} />
-      </div>
-      <div className="input-group">
-        <input type="text" name="businessEmail" placeholder="Business Email" onChange={handleInputChange} />
-      </div>
-      <div className="input-group">
-        <input type="text" name="industry" placeholder="Industry" value={companyProfileData.industry} onChange={handleInputChange} />
-      </div>
-    </div>
-    <div className="grid-row">
-      <div className="input-group">
-        {/* Placeholder for any additional inputs */}
-      </div>
-      <div className="input-group">
-        {/* Placeholder for any additional inputs */}
-      </div>
-      <div className="input-group">
-        <input type="file" name="logo" onChange={handleFileChange} />
+      <div className="input-group" style={{ display: 'flex', flexDirection: 'row' }}>
+        <input type="text" name="AADHAR" placeholder="AADHAR" onChange={handleInputChange} />
+        <input type="text" name="DIN" placeholder="DIN" onChange={handleInputChange} />
+        <input type="text" name="state" placeholder="State" onChange={handleInputChange} />
+        <input type="text" name="nationality" placeholder="Nationality" onChange={handleInputChange} />
+        <input type="text" name="designation" placeholder="Designation" onChange={handleInputChange} />
       </div>
     </div>
-  </div>
-  ```
-  
-  
+  );
 }
 
 function BusinessLegalInfo() {
@@ -229,4 +223,4 @@ function ActionButtons() {
 
 
 
-export default App;
+export default App
