b185c18de62d63d5217ce3bafb0abf4902bed3ed
NeejaViju <neejaviju22@gmail.com>
2023-10-29 02:09:26 +0530

    changes styles and added nav bars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import 'font-awesome/css/font-awesome.min.css';
 
 
 // App Component
@@ -20,160 +20,146 @@ function App() {
 function NavBar() {
   return (
     <div className="navbar">
-      <img src="path/to/logo.png" alt="Logo" />
-      <input type="text" placeholder="Search..." />
-      <span className="notification-icon">🔔</span>
-      <select className="profile-dropdown">
-        <option>Profile</option>
-        <option>Logout</option>
-      </select>
+      <img src="src\Capture.PNG" alt="Logo" className="navbar-logo" />
+      
+      {/* Add Icon */}
+      <div className="add-icon-container">
+        <i className="fa fa-plus-circle" aria-hidden="true"></i>
+      </div>
+
+      <div className="navbar-search-container">
+        <i className="fa fa-search" aria-hidden="true"></i>
+        <input type="text" placeholder="Search..." className="navbar-search" />
+      </div>
+      
+      <button className="btn voucher-entry">VOUCHER ENTRY</button>
+      <button className="btn upgrade">UPGRADE</button>
+    
+
+      <i className="fa fa-bell" aria-hidden="true"></i>
+      <div className="profile-container">
+        
+        <select className="profile-dropdown">
+          <option>ABC Private Limited</option>
+      
+        </select>
+        <i className="fa fa-cog" aria-hidden="true"></i>
+        <i className="fa fa-bars" aria-hidden="true"></i>
+      </div>
     </div>
   );
 }
+
+
 
 // SideBar Component
 function SideBar() {
-  return (
-    <div className="sidebar">
-      <button>Dashboard</button>
-      <button>Masters</button>
-      <button>Sales</button>
-      <button>purchases</button>
-      <button>Expenses</button>
-      <button>Banking</button>
-      <button>Approvels</button>
-      <button>Accounts</button>
-      <button>reports</button>
-      <button>e-way Bills</button>
-      {/* Add other menu options... */}
-    </div>
-  );
-}
-
-// MainContent Component
-function MainContent() {
-  return (
-    <div className="main-content">
-      <Tabs />
-      <CompanyProfile />
-      <Addresses />
-      <PersonalDetails />
-      <BusinessLegalInfo />
-      <BankAccountDetails />
-      <SigningContactPerson />
-      <ActionButtons />
-    </div>
-  );
-}
-
-// Other Components...
-function Tabs() {
-  return <div>Tabs Component Here...</div>;
-}
-
-function CompanyProfile() {
-  const [companyProfileData, setCompanyProfileData] = useState({
-      companyName: '',
-      registrationStatus: 'Registered',
-      companyType: '',
-      businessEntityName: '',
-      industry: '',
-      logo: null
-  });
-
-  const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setCompanyProfileData(prevData => ({
-          ...prevData,
-          [name]: value
-      }));
-  };
-
-  const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      setCompanyProfileData(prevData => ({
-          ...prevData,
-          logo: file
-      }));
-  };
+  const [showSubmenu, setShowSubmenu] = useState(false);
 
   return (
-      <div className="company-profile">
-          <h2>Company Profile</h2>
-          <div className="grid-row">
-              <div className="input-group">
-                  <input type="text" name="companyType" placeholder="Company Type" value={companyProfileData.companyType} onChange={handleInputChange} />
-              </div>
-              <div className="input-group">
-                  <input type="radio" name="registrationStatus" value="Registered" checked={companyProfileData.registrationStatus === 'Registered'} onChange={handleInputChange} /> Registered
-                  <input type="radio" name="registrationStatus" value="Unregistered" checked={companyProfileData.registrationStatus === 'Unregistered'} onChange={handleInputChange} /> Unregistered
-              </div>
-              <div className="input-group">
-                  <input type="text" name="companyID" placeholder="Company ID" onChange={handleInputChange} />
-              </div>
+      <div className="sidebar">
+          <h3>📊 Dashboard</h3>
+
+          <div 
+              className="menu-item master"
+              onClick={() => setShowSubmenu(!showSubmenu)}
+          >
+              🛠 Masters
           </div>
-          <div className="grid-row">
-              <div className="input-group">
-                  <input type="text" name="GSTNumber" placeholder="GST Number" onChange={handleInputChange} />
+
+          {showSubmenu && (
+              <div className="submenu">
+                  <div>Company Profile</div>
+                  <div>Item Master</div>
+                  <div>Ledger Master</div>
+                  <div>Account Master</div>
+                  <div>Tax Master</div>
+                  <div>Employee Master</div>
+                  <div>Opening Balance</div>
+
+
+                  {/* ... Add the other submenu items similarly ... */}
               </div>
-              <div className="input-group">
-                  <input type="text" name="businessTradeName" placeholder="Business Trade Name" onChange={handleInputChange} />
-              </div>
-              <div className="input-group">
-                  <input type="text" name="businessLegalName" placeholder="Business Legal Name" onChange={handleInputChange} />
-              </div>
-          </div>
-          <div className="grid-row">
-              <div className="input-group">
-                  <input type="text" name="mobileNumber" placeholder="Mobile Number" onChange={handleInputChange} />
-              </div>
-              <div className="input-group">
-                  <input type="text" name="businessEmail" placeholder="Business Email" onChange={handleInputChange} />
-              </div>
-              <div className="input-group">
-                  <input type="text" name="industry" placeholder="Industry" value={companyProfileData.industry} onChange={handleInputChange} />
-              </div>
-          </div>
-          <div className="grid-row">
-              <div className="input-group">
-                  {/* Placeholder for any additional inputs */}
-              </div>
-              <div className="input-group">
-                  {/* Placeholder for any additional inputs */}
-              </div>
-              <div className="input-group">
-                  <input type="file" name="logo" onChange={handleFileChange} />
-              </div>
-          </div>
+          )}
+
+          <div className="menu-item">🛒 Sales</div>
+          <div className="menu-item">📦 Purchases</div>
+          <div className="menu-item">💼 Expenses</div>
+          <div className="menu-item">🏦 Banking</div>
+          <div className="menu-item">✅ Approvals</div>
+          <div className="menu-item">💹 Accounts</div>
+          {/* ... Repeat similar divs for the other sections ... */}
+          <div className="menu-item">📊 Reports</div>
+          <div className="menu-item">📃 e-Way Bills</div>
+          <div className="menu-item">📜 e-Invoices</div>
+          <div className="menu-item">📋 Task</div>
+          <div className="menu-item">🎉 Promotions</div>
+          <div className="menu-item">📁 Tax Filings</div>
+          <div className="menu-item">📄 Subscriptions</div>
       </div>
   );
 }
 
 
-function Addresses() {
-  return <div>Addresses Component Here...</div>;
-}
-
-function PersonalDetails() {
-  return <div>PersonalDetails Component Here...</div>;
-}
-
-function BusinessLegalInfo() {
-  return <div>BusinessLegalInfo Component Here...</div>;
-}
-
-function BankAccountDetails() {
-  return <div>BankAccountDetails Component Here...</div>;
-}
-
-function SigningContactPerson() {
-  return <div>SigningContactPerson Component Here...</div>;
-}
-
-function ActionButtons() {
-  return <div>ActionButtons Component Here...</div>;
+// MainContent Component
+function MainContent() {
+  return (
+    <div className="main-content">
+      <CompanyProfileHeader/>
+      
+      {/* <Addresses />
+      <PersonalDetails />
+      <BusinessLegalInfo />
+      <BankAccountDetails />
+      <SigningContactPerson />
+      <ActionButtons /> */}
+    </div>
+  );
 }
 
 
+
+function CompanyProfileHeader() {
+  return (
+    <div className="profile-header">
+      <div className="profile-title">Company Profile</div>
+      <div className="company-type">
+        <span>Company Type</span>
+        <input type="radio" id="registered" name="companyType" value="registered" />
+        <label htmlFor="registered">Registered</label>
+        <input type="radio" id="unregistered" name="companyType" value="unregistered" />
+        <label htmlFor="unregistered">Unregistered</label>
+      </div>
+      <button className="back-btn">← Back</button>
+      <button className="save-exit-btn">Save & Exit</button>
+    </div>
+  );
+}
+
+
+
+// function Addresses() {
+//   return <div>Addresses Component Here...</div>;
+// }
+
+// function PersonalDetails() {
+//   return <div>PersonalDetails Component Here...</div>;
+// }
+
+// function BusinessLegalInfo() {
+//   return <div>BusinessLegalInfo Component Here...</div>;
+// }
+
+// function BankAccountDetails() {
+//   return <div>BankAccountDetails Component Here...</div>;
+// }
+
+// function SigningContactPerson() {
+//   return <div>SigningContactPerson Component Here...</div>;
+// }
+
+// function ActionButtons() {
+//   return <div>ActionButtons Component Here...</div>;
+// }
 
 export default App;
